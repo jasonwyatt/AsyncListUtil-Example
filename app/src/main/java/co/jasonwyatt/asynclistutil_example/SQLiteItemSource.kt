8173b340334a3c27e91f22148a5fca6d8827f3f6
Jason Feinstein <jason.feinstein@gmail.com>
2017-09-12 09:53:40 +0200

    Refactor a bit to use a property rather than a method for cursor.

--- app/src/main/java/co/jasonwyatt/asynclistutil_example/SQLiteItemSource.kt
+++ app/src/main/java/co/jasonwyatt/asynclistutil_example/SQLiteItemSource.kt
@@ -6,19 +6,20 @@ import android.database.sqlite.SQLiteDatabase
 
 class SQLiteItemSource(val database: SQLiteDatabase) : ItemSource {
     private var _cursor: Cursor? = null
-    @SuppressLint("Recycle")
-    private fun cursor(): Cursor {
-        if (_cursor == null || _cursor?.isClosed != false) {
-            _cursor = database.rawQuery("SELECT title, content FROM data", null)
+    private val cursor: Cursor
+        @SuppressLint("Recycle")
+        get() {
+            if (_cursor == null || _cursor?.isClosed != false) {
+                _cursor = database.rawQuery("SELECT title, content FROM data", null)
+            }
+            return _cursor ?: throw AssertionError("Set to null or closed by another thread")
         }
-        return _cursor ?: throw AssertionError("Set to null or closed by another thread")
-    }
 
-    override fun getCount() = cursor().count
+    override fun getCount() = cursor.count
 
     override fun getItem(position: Int): Item {
-        cursor().moveToPosition(position)
-        return Item(cursor())
+        cursor.moveToPosition(position)
+        return Item(cursor)
     }
 
     override fun close() {
